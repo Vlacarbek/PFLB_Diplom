40eb3410646bdebb4202f7c724f55f470fa1fb8a
Vershinina Alina <Kirillastrov@gmail.com>
2025-02-18 14:44:00 +0700

    Add method getToken and connect RestAssured

--- src/test/java/API/LoginTest.java
+++ src/test/java/API/LoginTest.java
@@ -1,4 +1,27 @@
 package API;
+import groovy.json.JsonException;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class LoginTest {
+
+    //Корректная авторизация, возвращает Bearer token
+    public static  String  GetToken() throws JsonException {
+        RestAssured.baseURI = "http://82.142.167.37:4879";
+        String requestBody = "{"
+                + "\"username\": \"user@pflb.ru\","
+                + "\"password\": \"user\""
+                + "}";
+        Response  response = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("host", "82.142.167.37")
+                .body(requestBody)
+                .when()
+                .post("/login")
+                .then()
+                .extract().response();
+        String  token = response.jsonPath().getString("access_token");
+        return token;
+
+    }
 }
