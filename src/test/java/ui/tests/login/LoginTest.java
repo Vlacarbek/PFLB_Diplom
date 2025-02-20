23dd4b82c691182e34d6ac81c7df0b65a3a7f73c
Vershinina Alina <Kirillastrov@gmail.com>
2025-02-21 02:46:31 +0700

    add tests for Login Ui

--- src/test/java/ui/tests/login/LoginTest.java
+++ src/test/java/ui/tests/login/LoginTest.java
@@ -14,7 +14,27 @@ LoginTest extends BaseTest {
         loginPage.login(user, password);
         Assert.assertEquals(loginPage.checkAut(), "Status: not pushed");
     }
+
+    //Негативная авторизация c пустой почтой
+    @Test
+    public void emptyEmail()  {
+        loginPage.open();
+        loginPage.login("  ", "test123");
+        Assert.assertTrue(loginPage.checkErrorText("email cannot be empty"));
+    }
+
+    //Негативная авторизация c некорректной почтой
+    @Test
+    public void invalidEmail()  {
+        loginPage.open();
+        loginPage.login("1111", "test123");
+        Assert.assertTrue(loginPage.checkErrorText("incorrect Email"));
+    }
+    //Негативная авторизация c некорректным паролем
+    @Test
+    public void invalidPassword()  {
+        loginPage.open();
+        loginPage.login("test1@test.com", "12");
+        Assert.assertTrue(loginPage.checkErrorText("password length must be more than 3 symbols and less than 8 symbols"));
+    }
 }
-
-
-
