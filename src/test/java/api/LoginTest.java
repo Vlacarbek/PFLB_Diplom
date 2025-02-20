7bb892a3be083da5c9f953e3d133acccdf3e6ccb
Vershinina Alina <Kirillastrov@gmail.com>
2025-02-21 03:04:25 +0700

    add tests for Login API

--- src/test/java/api/LoginTest.java
+++ src/test/java/api/LoginTest.java
@@ -4,20 +4,23 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.PropertyReader;
 
 public class LoginTest {
     static String requestBody;
     static Response response;
     static String token;
+    public static String user = System.getProperty("user", PropertyReader.getProperty("user"));
+    public static String password = System.getProperty("password", PropertyReader.getProperty("password"));
 
     //Корректная авторизация, возвращает Bearer tokens
     public static String GetToken() throws JsonException {
         RestAssured.baseURI = "http://82.142.167.37:4879";
-         requestBody = "{"
-                + "\"username\": \"user@pflb.ru\","
-                + "\"password\": \"user\""
+        requestBody = "{"
+                + "\"username\" : \"" + user + "\","
+                + "\"password\": \"" + password + "\""
                 + "}";
-         response = RestAssured.given()
+        response = RestAssured.given()
                 .header("Content-Type", "application/json")
                 .header("host", "82.142.167.37")
                 .body(requestBody)
@@ -25,7 +28,7 @@ public class LoginTest {
                 .post("/login")
                 .then()
                 .extract().response();
-         token = response.jsonPath().getString("access_token");
+        token = response.jsonPath().getString("access_token");
         return token;
     }
 
@@ -33,9 +36,10 @@ public class LoginTest {
     @Test
     public static  void   CorrectLogin() throws JsonException {
         RestAssured.baseURI = "http://82.142.167.37:4879";
+
         String requestBody = "{"
-                + "\"username\": \"user@pflb.ru\","
-                + "\"password\": \"user\""
+                + "\"username\" : \"" + user + "\","
+                + "\"password\": \"" + password + "\""
                 + "}";
         Response  response = RestAssured.given()
                 .header("Content-Type", "application/json")
@@ -48,24 +52,24 @@ public class LoginTest {
         String  token = response.jsonPath().getString("access_token");
         Assert.assertEquals(token.length(), 203);
     }
-        //Проверяем авторизацию с пустыми данными
-        @Test
-        public  void  EmptyLogin () throws JsonException {
-            RestAssured.baseURI = "http://82.142.167.37:4879";
-            String requestBody2 = "{"
-                    + "\"username\": ,"
-                    + "\"password\": "
-                    + "}";
-            response = RestAssured.given()
-                    .header("Content-Type", "application/json")
-                    .header("host", "82.142.167.37")
-                    .body(requestBody2)
-                    .when()
-                    .post("/login")
-                    .then()
-                    .extract().response();
-            Assert.assertEquals(response.getStatusCode(),403);
-        }
+    //Проверяем авторизацию с пустыми данными
+    @Test
+    public  void  EmptyLogin () throws JsonException {
+        RestAssured.baseURI = "http://82.142.167.37:4879";
+        String requestBody2 = "{"
+                + "\"username\": ,"
+                + "\"password\": "
+                + "}";
+        response = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("host", "82.142.167.37")
+                .body(requestBody2)
+                .when()
+                .post("/login")
+                .then()
+                .extract().response();
+        Assert.assertEquals(response.getStatusCode(),403);
+    }
 
     //Проверяем авторизацию с не валидными данными
     @Test
