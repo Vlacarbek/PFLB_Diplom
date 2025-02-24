33288651b1609799fda7abf75a99288875db32da
Vershinina Alina <Kirillastrov@gmail.com>
2025-02-25 04:47:00 +0700

    delete comment

--- src/test/java/ui/tests/BaseTest.java
+++ src/test/java/ui/tests/BaseTest.java
@@ -55,7 +55,7 @@ public class BaseTest {
         if (browser.equalsIgnoreCase("chrome")) {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("start-maximized");
-            //options.addArguments("--headless");
+            options.addArguments("--headless");
             options.setCapability("unhandledPromptBehavior", "accept");
             driver = new ChromeDriver(options);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
