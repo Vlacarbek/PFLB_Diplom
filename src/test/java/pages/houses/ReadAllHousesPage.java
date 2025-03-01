24e7724806a6648de5cf7325515cd815024f1370
Ruslan Mazitov <mazitov@mail.ru>
2025-03-01 21:23:31 +0500

    Добавил тесты  4.1Read all      4.1.1	Отсутствие пустых полей      4.1.1	Reload   4.2 Read one by ID      4.2.1	Проверка по айди что все соответствует Read all

--- src/test/java/pages/houses/ReadAllHousesPage.java
+++ src/test/java/pages/houses/ReadAllHousesPage.java
@@ -1,4 +1,70 @@
 package pages.houses;
 
-public class ReadAllHousesPage {
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import tests.BaseTest;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+public class ReadAllHousesPage extends BaseTest {
+
+    static WebDriver driver;
+
+    public static By housePage = By.xpath("//a[text()='Houses']");
+    public static By readAll = By.xpath("//a[@href='#/read/houses']");
+    public static By reloadButton = By.xpath("//button[text()='Reload']");
+    public static By lodgersIDReadAll = By.xpath("//*[@id='root']/div/section/div/table/tbody/tr[1]/td[5]/table/tbody/tr/td");
+
+    public ReadAllHousesPage(WebDriver driver) {
+        ReadAllHousesPage.driver = driver;
+    }
+
+    @Step("Открытие страницы ReadAllHousesPage")
+    public static void openReadAllHousesPage() {
+        driver.findElement(housePage).click();
+        driver.findElement(readAll).click();
+    }
+
+    @Step("Поиск пустых полей на странице ReadAllHousesPage")
+    public static Boolean fieldsNotNull() {
+        ArrayList<WebElement> allField = new ArrayList<>(driver.findElements(By.xpath("//td")));
+        List<String> stringField = new ArrayList<>();
+        for (WebElement field : allField) {
+            stringField.add(field.getText());
+        }
+        return stringField.stream().anyMatch(Objects::isNull);
+    }
+
+    @Step("Проверка кнопки Reload на странице ReadAllHousesPage: меняется ли сортировка")
+    public static Boolean buttonIsWorked() {
+        ArrayList<WebElement> allFieldFirstEqual = new ArrayList<>(driver.findElements(By.xpath("//td")));
+        List<String> stringFieldFirst = new ArrayList<>();
+        for (WebElement field : allFieldFirstEqual) {
+            stringFieldFirst.add(field.getText());
+        }
+        driver.findElement(reloadButton).click();
+        ArrayList<WebElement> allFieldSecondEqual = new ArrayList<>(driver.findElements(By.xpath("//td")));
+        List<String> stringFieldSecond = new ArrayList<>();
+        for (WebElement field : allFieldSecondEqual) {
+            stringFieldSecond.add(field.getText());
+        }
+        System.out.println(stringFieldFirst.equals(stringFieldSecond));
+        return stringFieldFirst.equals(stringFieldSecond);
+    }
+
+    @Step("Получение информации по жильцам на странице ReadAllHousesPage")
+    public static List<String> lodgersReadAllHousePage() {
+        List<String> lodgerslistReadAllPage = new ArrayList<>(driver.findElements(lodgersIDReadAll))
+                .stream()
+                .map(WebElement::getText)
+                .sorted(Comparator.naturalOrder())
+                .toList();
+        return lodgerslistReadAllPage;
+    }
 }
+
--- src/test/java/pages/houses/ReadOneByIDPage.java
+++ src/test/java/pages/houses/ReadOneByIDPage.java
@@ -1,8 +1,48 @@
 package pages.houses;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import tests.BaseTest;
 
-public class ReadOneByIDPage {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ReadOneByIDPage extends BaseTest {
+
+    static WebDriver driver;
+
+    public static By readOneByIDPage = By.xpath("//a[@href='#/read/house']");
+    public static By sendKeysField = By.xpath("//input[@type ='number']");
+    public static By readButton = By.xpath("//button[@class='tableButton btn btn-primary']");
+    public static By lodgersID = By.xpath("//*[@id='root']/div/section/div/table[2]/tbody/tr/td");
+
     public ReadOneByIDPage(WebDriver driver) {
+        ReadOneByIDPage.driver = driver;
+    }
+
+    @Step("Открытие страницы ReadOneByIDPage")
+    public static void openReadOneByIDPage() {
+        driver.findElement(ReadAllHousesPage.housePage).click();
+        driver.findElement(readOneByIDPage).click();
+    }
+
+    @Step("Ввод числа в поле house_input числа: {IDNumber} и нажатие кнопки Read")
+    public static void clickReadButton(String IDNumber) {
+        driver.findElement(sendKeysField).click();
+        driver.findElement(sendKeysField).sendKeys(IDNumber);
+        driver.findElement(readButton).click();
+    }
+
+    @Step("Получение информации по жильцам на странице ReadOneByIDPage")
+    public static List<String> lodgersReadOneByIDPage() {
+        List<String> lodgerslist = new ArrayList<>(driver.findElements(lodgersID))
+                .stream()
+                .map(WebElement::getText)
+                .sorted(Comparator.naturalOrder())
+                .toList();
+        return lodgerslist;
     }
 }
