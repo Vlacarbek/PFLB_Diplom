48d2ce844e88a9edf98bef0f7b2b68183a70d6ad
Ruslan Mazitov <mazitov@mail.ru>
2025-03-04 04:42:54 +0500

    закомментировал в LoggerTest fatal, он его даже не находит почему то

--- src/test/java/tests/LoggerTest.java
+++ src/test/java/tests/LoggerTest.java
@@ -1,15 +1,19 @@
 package tests;
 
 import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 
 @Log4j2
 public class LoggerTest {
 
+    private static final Logger log = LoggerFactory.getLogger(LoggerTest.class);
+
     @Test
     public void test() {
-        log.fatal("fatal");
+//        log.fatal("fatal");
         log.error("error");
         log.warn("warn");
         log.info("");
