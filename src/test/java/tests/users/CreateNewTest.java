2c3d4f81ad1a72b4ae6cc8c89b5f3a8eaacd3b0e
Ruslan Mazitov <mazitov@mail.ru>
2025-03-06 20:20:34 +0500

    паскоментировал юзеров

--- src/test/java/tests/users/CreateNewTest.java
+++ src/test/java/tests/users/CreateNewTest.java
@@ -8,50 +8,50 @@ import static org.testng.Assert.assertEquals;
 
 public class CreateNewTest extends BaseTest {
 
-//    @Test(testName = "Корректное создание юзера",
-//            description = "Позитивная проверка на корректное создание юзера")
-//    @Severity(SeverityLevel.CRITICAL)
-//    @Epic("PFLB 1.0")
-//    @Feature("CreateUser")
-//    @Story("Create User")
-//    @TmsLink("www.jira.com/Cu-1")
-//    public static void createUserCorrectData() throws InterruptedException {
-//        LoginPage.open();
-//        LoginPage.login(user, password);
-//        CreateNewUsersPage.open();
-//        CreateNewUsersPage.fillFields("Alina", "Vershinina", "19", "19");
-//        CreateNewUsersPage.clickButton();
-//        assertEquals(CreateNewUsersPage.checkResultText(), "Status: Successfully pushed, code: 201");
-//    }
-//
-//    @Test(testName = "Cоздание юзера с невалидными значениями",
-//            description = "Негативная проверка на создание юзера с невалидными значениями")
-//    @Severity(SeverityLevel.NORMAL)
-//    @Epic("PFLB 1.0")
-//    @Feature("CreateUser")
-//    @Story("Create User")
-//    @TmsLink("www.jira.com/Cu-1")
-//    public static void createUserInvalidData() throws InterruptedException {
-//        LoginPage.open();
-//        LoginPage.login(user, password);
-//        CreateNewUsersPage.open();
-//        CreateNewUsersPage.fillFields("56", "56", "4444444", "354454353543543354454353543543354454353543543");
-//        CreateNewUsersPage.clickButton();
-//        assertEquals(CreateNewUsersPage.checkResultText(), "Status: AxiosError: Request failed with status code 400");
-//    }
-//
-//    @Test(testName = "Cоздание юзера с пустыми значениями",
-//            description = "Негативная проверка на создание юзера с пустыми значениями")
-//    @Severity(SeverityLevel.NORMAL)
-//    @Epic("PFLB 1.0")
-//    @Feature("CreateUser")
-//    @Story("Create User")
-//    @TmsLink("www.jira.com/Cu-1")
-//    public static void createUserEmptyData() throws InterruptedException {
-//        LoginPage.open();
-//        LoginPage.login(user, password);
-//        CreateNewUsersPage.open();
-//        CreateNewUsersPage.clickButton();
-//        assertEquals(CreateNewUsersPage.checkResultText(), "Status: Invalid request data");
-//    }
+    @Test(testName = "Корректное создание юзера",
+            description = "Позитивная проверка на корректное создание юзера")
+    @Severity(SeverityLevel.CRITICAL)
+    @Epic("PFLB 1.0")
+    @Feature("CreateUser")
+    @Story("Create User")
+    @TmsLink("www.jira.com/Cu-1")
+    public static void createUserCorrectData() throws InterruptedException {
+        LoginPage.open();
+        LoginPage.login(user, password);
+        CreateNewUsersPage.open();
+        CreateNewUsersPage.fillFields("Alina", "Vershinina", "19", "19");
+        CreateNewUsersPage.clickButton();
+        assertEquals(CreateNewUsersPage.checkResultText(), "Status: Successfully pushed, code: 201");
+    }
+
+    @Test(testName = "Cоздание юзера с невалидными значениями",
+            description = "Негативная проверка на создание юзера с невалидными значениями")
+    @Severity(SeverityLevel.NORMAL)
+    @Epic("PFLB 1.0")
+    @Feature("CreateUser")
+    @Story("Create User")
+    @TmsLink("www.jira.com/Cu-1")
+    public static void createUserInvalidData() throws InterruptedException {
+        LoginPage.open();
+        LoginPage.login(user, password);
+        CreateNewUsersPage.open();
+        CreateNewUsersPage.fillFields("56", "56", "4444444", "354454353543543354454353543543354454353543543");
+        CreateNewUsersPage.clickButton();
+        assertEquals(CreateNewUsersPage.checkResultText(), "Status: AxiosError: Request failed with status code 400");
+    }
+
+    @Test(testName = "Cоздание юзера с пустыми значениями",
+            description = "Негативная проверка на создание юзера с пустыми значениями")
+    @Severity(SeverityLevel.NORMAL)
+    @Epic("PFLB 1.0")
+    @Feature("CreateUser")
+    @Story("Create User")
+    @TmsLink("www.jira.com/Cu-1")
+    public static void createUserEmptyData() throws InterruptedException {
+        LoginPage.open();
+        LoginPage.login(user, password);
+        CreateNewUsersPage.open();
+        CreateNewUsersPage.clickButton();
+        assertEquals(CreateNewUsersPage.checkResultText(), "Status: Invalid request data");
+    }
 }
